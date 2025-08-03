0f5c5367a570083331e83f72b3393dc816ca3de8
Jon Connell <python@figsandfudge.com>
2025-08-03 17:32:32 +0100

    Remove Debug prints

--- src/gui_paint_extra.c
+++ src/gui_paint_extra.c
@@ -15,26 +15,18 @@ int Paint_DrawVariableWidthChar(UWORD Xpoint, UWORD Ypoint, const char Acsii_Cha
     int font_height = Font->Height;
     int font_width = entry->Width;
 
-    Debug("Paint_DrawVariableWidthChar: character '%c', font_height=%d\r\n", Acsii_Char, font_height);
-
     for (Page = 0; Page < Font->Height; Page++) {
         for (Column = 0; Column < font_width; Column++) {
             // To determine whether the font background color and screen background
             // color is consistent
             if (FONT_BACKGROUND == Color_Background) { // this process is to speed up the scan
                 if (*ptr & (0x80 >> (Column % 8))) {
-                    Debug("Paint_DrawVariableWidthChar: [%d,%d] 0x%04x\r\n", Xpoint + Column, Ypoint + Page,
-                          Color_Foreground);
                     Paint_SetPixel(Xpoint + Column, Ypoint + Page, Color_Foreground);
                 }
             } else {
                 if (*ptr & (0x80 >> (Column % 8))) {
-                    Debug("Paint_DrawVariableWidthChar: [%d,%d] 0x%04x\r\n", Xpoint + Column, Ypoint + Page,
-                          Color_Foreground);
                     Paint_SetPixel(Xpoint + Column, Ypoint + Page, Color_Foreground);
                 } else {
-                    Debug("Paint_DrawVariableWidthChar: [%d,%d] 0x%04x\r\n", Xpoint + Column, Ypoint + Page,
-                          Color_Background);
                     Paint_SetPixel(Xpoint + Column, Ypoint + Page, Color_Background);
                 }
             }
@@ -44,7 +36,6 @@ int Paint_DrawVariableWidthChar(UWORD Xpoint, UWORD Ypoint, const char Acsii_Cha
         ptr += Font->Width - font_width / 8; // Move to next line
     } // Write all
 
-    Debug("Paint_DrawVariableWidthChar: last width %d\n", font_width);
     return font_width;
 }
 
@@ -56,8 +47,6 @@ void Paint_DrawVariableWidthString(UWORD Xstart, UWORD Ystart, const char *pStri
     Debug("Paint_DrawVariableWidthString: starting\r\n");
 
     if (Xstart > Paint.Width || Ystart > Paint.Height) {
-        Debug("Paint_DrawVariableWidthString Input exceeds the normal display "
-              "range\r\n");
         return;
     }
 
