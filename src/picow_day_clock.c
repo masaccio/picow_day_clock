ff1cfa71c2ac51b669f9dce37e96f94f39813ec2
Jon Connell <python@figsandfudge.com>
2025-09-01 10:35:15 +0100

    Moved wifi code to new module

--- src/picow_day_clock.c
+++ src/picow_day_clock.c
@@ -18,7 +18,6 @@
 
 /* Pico SDK */
 #include "lwip/dns.h"
-#include "pico/cyw43_arch.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
 
@@ -28,13 +27,7 @@
 #include "gui_paint_extra.h"
 #include "lcd.h"
 #include "ntp.h"
-
-#include "Debug.h" /* Waveshare */
-
-#define WIFI_CONNECT_TIMEOUT_MS (10 * 1000)
-#define WIFI_ABANDON_TIMEOUT_MS (60 * 1000)
-#define WIFI_BAD_AUTH_RETRY_COUNT 3
-#define WIFI_BAD_AUTH_RETRY_DELAY_MS 500
+#include "wifi.h"
 
 typedef struct clock_state_t
 {
@@ -48,47 +41,6 @@ void ntp_timer_callback(void *state, time_t *ntp_time)
 {
     clock_state_t *clock_state = (clock_state_t *)state;
     clock_state->ntp_time = *ntp_time;
-}
-
-bool connect_to_wifi(const char ssid[], const char password[])
-{
-    if (cyw43_arch_init()) {
-        CLOCK_DEBUG("Wi-Fi: failed to initialise CYW43\r\n");
-        return 1;
-    }
-
-    cyw43_arch_enable_sta_mode();
-
-    absolute_time_t start_time = get_absolute_time();
-    int bad_auth_count = 0;
-    while (true) {
-        int ret = cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, WIFI_CONNECT_TIMEOUT_MS);
-
-        if (ret == 0) {
-            CLOCK_DEBUG("Wi-Fi: connected to %s\r\n", ssid);
-            return true;
-        } else if (ret == PICO_ERROR_TIMEOUT) {
-            if (absolute_time_diff_us(start_time, get_absolute_time()) > (WIFI_ABANDON_TIMEOUT_MS * 1000)) {
-                CLOCK_DEBUG("Wi-Fi: exceeded maximum timeout; giving up\r\n");
-                return false;
-            }
-            CLOCK_DEBUG("Wi-Fi: timeout; trying again\r\n");
-        } else if (ret == PICO_ERROR_BADAUTH) {
-            if (++bad_auth_count > WIFI_BAD_AUTH_RETRY_COUNT) {
-                CLOCK_DEBUG("Wi-Fi: too many bad authentication failures; giving up\r\n");
-                return false;
-            } else {
-                CLOCK_DEBUG("Wi-Fi: invalid credentials; retrying...\r\n");
-                sleep_ms(WIFI_BAD_AUTH_RETRY_DELAY_MS);
-            }
-        } else if (ret == PICO_ERROR_CONNECT_FAILED) {
-            CLOCK_DEBUG("Wi-Fi: connection failed for unknown reason; giving up\r\n");
-            return false;
-        } else {
-            CLOCK_DEBUG("Wi-Fi: unknown error %d; giving up\r\n", ret);
-            return false;
-        }
-    }
 }
 
 int main()
@@ -144,6 +96,6 @@ int main()
         sleep_ms(delay_ms);
     }
 
-    cyw43_arch_deinit();
+    disconnect_from_wifi();
     return 0;
 }
