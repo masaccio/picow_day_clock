3c1e55a015afe01bb213e6b126160dc59151137e
Jon Connell <python@figsandfudge.com>
2025-09-07 16:02:15 +0100

    Return wifi_status_t

--- src/wifi.c
+++ src/wifi.c
@@ -10,11 +10,10 @@
 #include "common.h"
 #include "wifi.h"
 
-int connect_to_wifi(const char ssid[], const char password[])
+wifi_status_t connect_to_wifi(const char ssid[], const char password[])
 {
     if (cyw43_arch_init()) {
-        CLOCK_DEBUG("Wi-Fi: failed to initialise CYW43\r\n");
-        return 1;
+        return WIFI_STATUS_INIT_FAIL;
     }
 
     cyw43_arch_enable_sta_mode();
@@ -26,30 +25,30 @@ int connect_to_wifi(const char ssid[], const char password[])
 
         if (ret == 0) {
             CLOCK_DEBUG("Wi-Fi: connected to %s\r\n", ssid);
-            return 0;
+            return WIFI_STATUS_SUCCESS;
         } else if (ret == PICO_ERROR_TIMEOUT) {
             if (absolute_time_diff_us(start_time, get_absolute_time()) > (WIFI_ABANDON_TIMEOUT_MS * 1000)) {
                 CLOCK_DEBUG("Wi-Fi: exceeded maximum timeout; giving up\r\n");
-                return 1;
+                return WIFI_STATUS_TIMEOUT;
             }
             CLOCK_DEBUG("Wi-Fi: timeout; trying again\r\n");
         } else if (ret == PICO_ERROR_BADAUTH) {
-            if (++bad_auth_count > WIFI_BAD_AUTH_RETRY_COUNT) {
+            bad_auth_count++;
+            if (bad_auth_count > WIFI_BAD_AUTH_RETRY_COUNT) {
                 CLOCK_DEBUG("Wi-Fi: too many bad authentication failures; giving up\r\n");
-                return 1;
+                return WIFI_STATUS_BAD_AUTH;
             } else {
                 CLOCK_DEBUG("Wi-Fi: invalid credentials; retrying...\r\n");
                 sleep_ms(WIFI_BAD_AUTH_RETRY_DELAY_MS);
             }
         } else if (ret == PICO_ERROR_CONNECT_FAILED) {
             CLOCK_DEBUG("Wi-Fi: connection failed for unknown reason; giving up\r\n");
-            return 1;
+            return WIFI_STATUS_CONNECT_FAILED;
         } else {
             CLOCK_DEBUG("Wi-Fi: unknown error %d; giving up\r\n", ret);
-            return 1;
+            return WIFI_STATUS_UNKNOWN_ERROR;
         }
     }
-    return 0;
 }
 
 void disconnect_from_wifi(void)
--- src/wifi.h
+++ src/wifi.h
@@ -3,12 +3,22 @@
 
 #include <stdbool.h>
 
-#define WIFI_CONNECT_TIMEOUT_MS (10 * 1000)
-#define WIFI_ABANDON_TIMEOUT_MS (60 * 1000)
-#define WIFI_BAD_AUTH_RETRY_COUNT 3
-#define WIFI_BAD_AUTH_RETRY_DELAY_MS 500
+#define WIFI_CONNECT_TIMEOUT_MS (10 * 1000) /* Time to allow Wi-Fi driver to connect */
+#define WIFI_ABANDON_TIMEOUT_MS (60 * 1000) /* How long to keep retrying connection */
+#define WIFI_BAD_AUTH_RETRY_COUNT 3         /* How many bad auth errors to tolerate */
+#define WIFI_BAD_AUTH_RETRY_DELAY_MS 500    /* Time to wait before retrying after auth error */
 
-extern int connect_to_wifi(const char ssid[], const char password[]);
+typedef enum
+{
+    WIFI_STATUS_SUCCESS = 0,
+    WIFI_STATUS_INIT_FAIL = -1,
+    WIFI_STATUS_TIMEOUT = -2,
+    WIFI_STATUS_BAD_AUTH = -3,
+    WIFI_STATUS_CONNECT_FAILED = -4,
+    WIFI_STATUS_UNKNOWN_ERROR = -5,
+} wifi_status_t;
+
+extern wifi_status_t connect_to_wifi(const char ssid[], const char password[]);
 
 extern void disconnect_from_wifi(void);
 
