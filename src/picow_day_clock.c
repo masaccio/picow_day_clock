a6732ecd7022cd036d6983bb25e6a0394a419532
Jon Connell <python@figsandfudge.com>
2025-12-12 16:28:42 +0000

    Debugging Windows build

--- src/picow_day_clock.c
+++ src/picow_day_clock.c
@@ -228,7 +228,10 @@ static void set_time_of_day(clock_state_t *state)
 {
     struct timeval tv;
 #ifdef _WIN32
-    tv.tv_sec = (long)(int32_t)state->ntp_time;
+    _Static_assert(sizeof(state->ntp_time) == 4, "ntp_time size");
+    _Static_assert(_Generic((state->ntp_time), uint32_t: 1, int32_t: 2, default: 0) == 1, "ntp_time type");
+    _Static_assert(sizeof(((struct timeval *)0)->tv_sec) == 4, "tv_sec size");
+    tv.tv_sec = (long)state->ntp_time;
     tv.tv_usec = (long)0;
 #else
     tv.tv_sec = state->ntp_time;
