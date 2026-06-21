3d0acd906c2d06f783985525d9ee71bdc7d23847
Jon Connell <python@figsandfudge.com>
2026-06-21 11:53:19 +0100

    Initial commit

---
+++ clean_build.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+if [ $(uname -s) == "Darwin" ]; then
+  brew update
+  brew install picotool llvm
+  brew install --cask gcc-arm-embedded
+
+  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.2.0"
+  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1"
+  export PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/picotool/2.2.0/picotool:${PATH}"
+  export PATH="${HOME}/.pico-sdk/cmake/v3.31.5/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/ninja/v1.12.1:${PATH}"
+  export PATH="/opt/homebrew/opt/llvm/bin:${PATH}"
+  export PATH="$(brew --prefix gcc-arm-embedded)/bin:${PATH}"
+  export PATH="$(brew --prefix llvm)/bin:${PATH}"
+else
+  sudo apt-get update
+  sudo apt-get install -y cmake ninja-build lcov llvm gcc 
+  sudo apt-get install -y gcc-arm-none-eabi libnewlib-arm-none-eabi
+
+  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.2.0"
+  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1"
+  export PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/picotool/2.2.0/picotool:${PATH}"
+  export PATH="${HOME}/.pico-sdk/cmake/v3.31.5/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/ninja/v1.12.1:${PATH}"
+fi
+
+rm -rf build
+mkdir -p build
+
+cmake -DCMAKE_BUILD_TYPE=Debug -B build -G Ninja
+ninja -v -C build test_picow_day_clock
+cmake --build build --target run_tests
+cmake --build build --target coverage_llvm
