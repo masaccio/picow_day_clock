622313568a9f0be24e69529cbddf47955d139c7e
Jon Connell <python@figsandfudge.com>
2026-07-04 14:11:20 +0100

    Upgrade SDK to 2.3.0

--- clean_build.sh
+++ clean_build.sh
@@ -4,12 +4,12 @@ if [ $(uname -s) == "Darwin" ]; then
   brew install picotool llvm
   brew install --cask gcc-arm-embedded
 
-  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.2.0"
-  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1"
-  export PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1/bin:${PATH}"
-  export PATH="${HOME}/.pico-sdk/picotool/2.2.0/picotool:${PATH}"
-  export PATH="${HOME}/.pico-sdk/cmake/v3.31.5/bin:${PATH}"
-  export PATH="${HOME}/.pico-sdk/ninja/v1.12.1:${PATH}"
+  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.3.0"
+  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/15_2_Rel1"
+  export PATH="${HOME}/.pico-sdk/toolchain/15_2_Rel1/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/picotool/2.3.0/picotool:${PATH}"
+  export PATH="${HOME}/.pico-sdk/cmake/v4.3.4/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/ninja/v1.13.2:${PATH}"
   export PATH="/opt/homebrew/opt/llvm/bin:${PATH}"
   export PATH="$(brew --prefix gcc-arm-embedded)/bin:${PATH}"
   export PATH="$(brew --prefix llvm)/bin:${PATH}"
@@ -18,12 +18,12 @@ else
   sudo apt-get install -y cmake ninja-build lcov llvm gcc 
   sudo apt-get install -y gcc-arm-none-eabi libnewlib-arm-none-eabi
 
-  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.2.0"
-  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1"
-  export PATH="${HOME}/.pico-sdk/toolchain/14_2_Rel1/bin:${PATH}"
-  export PATH="${HOME}/.pico-sdk/picotool/2.2.0/picotool:${PATH}"
-  export PATH="${HOME}/.pico-sdk/cmake/v3.31.5/bin:${PATH}"
-  export PATH="${HOME}/.pico-sdk/ninja/v1.12.1:${PATH}"
+  export PICO_SDK_PATH="${HOME}/.pico-sdk/sdk/2.3.0"
+  export PICO_TOOLCHAIN_PATH="${HOME}/.pico-sdk/toolchain/15_2_Rel1"
+  export PATH="${HOME}/.pico-sdk/toolchain/15_2_Rel1/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/picotool/2.3.0/picotool:${PATH}"
+  export PATH="${HOME}/.pico-sdk/cmake/v4.3.4/bin:${PATH}"
+  export PATH="${HOME}/.pico-sdk/ninja/v1.13.2:${PATH}"
 fi
 
 rm -rf build
